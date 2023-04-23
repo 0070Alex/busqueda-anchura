7f7ef2afdab2456e949f121c272655d12e6f2d48
alex <78516248+0070Alex@users.noreply.github.com>
2023-04-22 22:08:30 -0600

    Se versiona proyecto java

---
+++ ActividadBusqueda/src/actividadbusqueda/ActividadBusqueda.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividadbusqueda;
+
+/**
+ *
+ * @author Alexander Elias
+ */
+public class ActividadBusqueda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public void search(Laberinto l){
+    
+       // Nodo nodo = new Nodo(); 
+        
+    
+    };
+}
